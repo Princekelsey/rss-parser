b91312f773f519677b4f86a1e5d03be9c0f8f850
Prince Ajuobi <princekelsey299@gmail.com>
2020-08-27 00:27:27 +0300

    added proxy

--- src/api/Server.ts
+++ src/api/Server.ts
@@ -1,9 +1,11 @@
 const Parser = require("rss-parser");
 const parser = new Parser();
 
+const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+
 const Server = {
   getRSSFeeds: async (url: string) => {
-    return await parser.parseURL(url);
+    return await parser.parseURL(CORS_PROXY + url);
   },
 };
 
